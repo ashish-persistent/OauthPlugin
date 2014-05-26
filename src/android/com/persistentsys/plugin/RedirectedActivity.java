eae1943a78b4239532a80477f61f8dfe8a6e33d6
ashish-persistent <ashishasolanki@Ashishs-iMac.local>
2014-05-26 11:38:21 +0530

    changes

--- src/android/com/persistentsys/plugin/RedirectedActivity.java
+++ src/android/com/persistentsys/plugin/RedirectedActivity.java
@@ -2,6 +2,7 @@ package com.persistentsys.plugin;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.persistent.sso.lib.PeasClientAuthenticationException;
 import com.persistent.sso.lib.PeasClientAuthenticator;
@@ -13,6 +14,7 @@ public class RedirectedActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.v("iGreet", "iGreet: RedirectedActivity");
 
 		try {
 			PeasClientAuthenticator.getAuthenticationHandler().getAccessToken(this);
