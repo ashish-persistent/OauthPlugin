07105a960a96b0098397d73d48575c3dfce750ea
ashish-persistent <ashishasolanki@Ashishs-iMac.local>
2014-05-27 16:17:21 +0530

    changes

--- www/oAuthPlugin.js
+++ www/oAuthPlugin.js
@@ -1,8 +1,8 @@
 var oAuthPlugin = {
 
-	callNativeFunction : function(success, error, options) {
-		// alert("callNativeFunction");
-		cordova.exec(success, error, 'OauthPlugin', 'getToken', [options]);
+	callNativeFunction : function(success, error, oauth_type, options) {
+		alert("callNativeFunction");
+		cordova.exec(success, error, 'OauthPlugin', oauth_type, [options]);
 		// return Cordova.exec(success, fail, "OauthPlugin", "getToken", ["resultType","DataType","New Data"]);
 		// return cordova.exec(successCallback, failureCallback, "MyCustomSavePluginCommand","saveToDocumentsMethod",[data[0]]);
 	},
